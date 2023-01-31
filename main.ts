51ac06dd6d896996d0377eb1a91f7d2f065e7c48
Nathan Gerryts <36611779+Nathongols@users.noreply.github.com>
2023-01-31 16:02:21 -0500

    Morse to english converter

--- main.ts
+++ main.ts
@@ -1,42 +1,67 @@
-input.onButtonPressed(Button.A, function () {
-	
+
+
+const code = {
+    "._" : "A",
+    "_..." : "B",
+    "_._." : "C",
+    "_.." : "D",
+    "." : "E",
+    ".._." : "F",
+    "__." : "G",
+    "...." : "H",
+    ".." : "I",
+    ".___" : "J",
+    "_._" : "K",
+    "._.." : "L",
+    "__" : "M",
+    "_." : "N",
+    "___" : "O",
+    ".__." : "P",
+    "__._" : "Q",
+    "._." : "R",
+    "..." : "S",
+    "_" : "T",
+    ".._" : "U",
+    "..._" : "V",
+    ".__" : "W",
+    "_.._" : "X",
+    "_.__" : "Y",
+    "__.." : "Z",
+}
+
+let message = ""
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    
+    message = "" + message + "."
+    music.playTone(262, music.beat(BeatFraction.Half))
 })
-radio.onReceivedString(function (receivedString) {
-    music.playTone(262, music.beat(BeatFraction.Sixteenth))
-    led.plot(pointerX, pointerY)
-})
-input.onLogoEvent(TouchButtonEvent.Pressed, function () {
-    pointerX = 0
-    images.createBigImage(`
-        # . . . # . . . . .
-        . # . # . . . . . .
-        . . # . . . . . . .
-        . # . # . . . . . .
-        # . . . # . . . . .
-        `).scrollImage(1, 50)
-})
-let pointerY = 0
-let pointerX = 0
-pointerX = 0
-pointerY = 0
-radio.setGroup(1)
-basic.showLeds(`
-    # . . . .
-    . # . . .
-    . . # . .
-    . . . . .
-    . . . . .
-    `)
-loops.everyInterval(1000, function () {
-    pointerX += 1
-    if (pointerX >= 5) {
-        pointerX = 0
-        pointerY += 1
+function morse_to_string(code:{[key: string]:string, value: string},morse: string): string {
+    let word: string;
+    let result = ""
+    let words = _py.py_string_split(morse, " ")
+    for (let x of words) {
+        word = x
+        result += code[word] + " "
     }
+    return result
+}
+
+radio.onReceivedString(function on_received_string(receivedString: string) {
+    basic.showString(receivedString)
 })
-basic.forever(function () {
-    while (input.buttonIsPressed(Button.A)) {
-        music.playTone(262, music.beat(BeatFraction.Sixteenth))
-        radio.sendString("")
-    }
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    
+    message = "" + message + "_"
+    music.playTone(220, music.beat(BeatFraction.Whole))
+})
+input.onGesture(Gesture.Shake, function on_gesture_shake() {
+    
+    message = ""
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function on_logo_pressed() {
+    radio.sendString(message)
+    basic.showString(message)
+})
+basic.forever(function on_forever() {
+    
 })
